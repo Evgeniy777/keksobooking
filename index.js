a6b637886bafe588c30ea9d00a22238d304f290e
Evgenii Levashov <evgenii.levashov@gmail.com>
2018-02-11 20:23:15 +0300

    project setup

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const args = process.argv.slice(2);
 
-switch(args[0]) {
+switch (args[0]) {
   case `--version`:
-        console.log(`v0.0.1`);
-        break;
+    console.log(`v0.0.1`);
+    break;
   case `--help`:
-        console.log(`Комманды:
-        --version - версия приложения;
-        --help    - информаци о приложении;`);
-        break;
+    console.log(`Комманды:
+    --version - версия приложения;
+    --help    - информаци о приложении;`);
+    break;
   default:
-        console.log(`Привет пользователь!
+    console.log(`Привет пользователь!
 Эта программа будет запускать сервер «Кексобукинг».
 Автор: Cannibal.`);
-        console.error(`Чтобы прочитать правила использования приложения, наберите "--help"`);
+    console.error(`Чтобы прочитать правила использования приложения, наберите "--help"`);
 }
