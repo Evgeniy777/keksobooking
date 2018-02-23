23de7b04cd1a66fa05691f0ea93f15457af6337c
Evgenii Levashov <evgenii.levashov@gmail.com>
2018-02-23 19:00:17 +0300

    tests

--- test/generate-entity.test.js
+++ test/generate-entity.test.js
@@ -3,54 +3,71 @@ const {generateEntity} = require(`../src/generate-entity`);
 const DATA = require(`../src/data`);
 
 describe(`#generateEntity`, () => {
-  it(`should generate "author" section`, () => {
+  it(`generates "author" section`, () => {
     assert.ok(generateEntity().author);
   });
+});
 
-  it(`"author.avatar" is string type`, () => {
+describe(`"author.avatar" `, () => {
+  it(`is string type`, () => {
     assert.equal(typeof generateEntity().author.avatar, `string`);
   });
+});
 
-  it(`"offer.title" is one of TITLES`, () => {
+describe(`"offer.title"`, () => {
+  it(`is one of TITLES`, () => {
     const {offer: {title}} = generateEntity();
     assert.ok(DATA.TITLES.indexOf(title) > -1);
   });
+});
 
-  it(`"offer.address" has format like ({{x}},{{y}})`, () => {
-    assert.equal(generateEntity().offer.address, `{{${generateEntity().location.x}}}, {{${generateEntity().location.y}}}`);
+describe(`"offer.address"`, () => {
+  it(`has format like ({{x}},{{y}})`, () => {
+    const {offer: {address}} = generateEntity();
+    assert.equal(address, `{{${generateEntity().location.x}}}, {{${generateEntity().location.y}}}`);
   });
+});
 
-  it(`"offer.price" is between 1000 and 1 000 000`, () => {
+describe(`"offer.price"`, () => {
+  it(`is between 1000 and 1 000 000`, () => {
     const {offer: {price}} = generateEntity();
-
     assert.ok(price >= DATA.PRICE.MIN);
     assert.ok(price <= DATA.PRICE.MAX);
   });
+});
 
-  it(`"offer.rooms" is between 1 and 5`, () => {
+describe(`"offer.rooms"`, () => {
+  it(`is between 1 and 5`, () => {
     const {offer: {rooms}} = generateEntity();
-
     assert.ok(rooms >= DATA.ROOMS.MIN);
     assert.ok(rooms <= DATA.ROOMS.MAX);
   });
+});
 
-  it(`"offer.guests" is number > 0`, () => {
+describe(`"offer.guests"`, () => {
+  it(`is number > 0`, () => {
     const {offer: {guests}} = generateEntity();
     assert.equal(typeof guests, `number`);
     assert.ok(guests > 0);
   });
+});
 
-  it(`"offer.checkin" is one of TIME`, () => {
+describe(`"offer.checkin"`, () => {
+  it(`is one of TIME`, () => {
     const {offer: {checkin}} = generateEntity();
     assert.ok(DATA.TIME.indexOf(checkin) > -1);
   });
+});
 
-  it(`"offer.checkout" is one of TIME`, () => {
+describe(`"offer.checkout"`, () => {
+  it(`is one of TIME`, () => {
     const {offer: {checkout}} = generateEntity();
     assert.ok(DATA.TIME.indexOf(checkout) > -1);
   });
+});
 
-  it(`"offer.features" is array of random values from FEATURES`, () => {
+describe(`"offer.features"`, () => {
+  it(`is array of random values from FEATURES`, () => {
     const {offer: {features}} = generateEntity();
 
     assert.ok(features instanceof Array);
@@ -58,32 +75,37 @@ describe(`#generateEntity`, () => {
       assert.ok(DATA.FEATURES.indexOf(feature) > -1);
     });
   });
+});
 
-  it(`"offer.description" is string this length equal 0`, () => {
+describe(`"offer.description"`, () => {
+  it(`is string this length equal 0`, () => {
     const {offer: {description}} = generateEntity();
 
     assert.ok(typeof description === `string` && description.length === 0);
   });
+});
 
-  it(`"offer.photos" is array of random values from PHOTOS`, () => {
+describe(`"offer.photos"`, () => {
+  it(`is array of random values from PHOTOS`, () => {
     const {offer: {photos}} = generateEntity();
-
     assert.ok(photos instanceof Array);
     photos.forEach((photo) => {
       assert.ok(DATA.PHOTOS.indexOf(photo) > -1);
     });
   });
+});
 
-  it(`"location.x" is between 300 and 900`, () => {
+describe(`"location.x"`, () => {
+  it(`is between 300 and 900`, () => {
     const entity = generateEntity();
-
     assert.ok(entity.location.x >= DATA.LOCATION.X.MIN);
     assert.ok(entity.location.x <= DATA.LOCATION.X.MAX);
   });
+});
 
-  it(`"location.y" is between 150 and 500`, () => {
+describe(`"location.y" `, () => {
+  it(`is between 150 and 500`, () => {
     const entity = generateEntity();
-
     assert.ok(entity.location.y >= DATA.LOCATION.Y.MIN);
     assert.ok(entity.location.y <= DATA.LOCATION.Y.MAX);
   });
